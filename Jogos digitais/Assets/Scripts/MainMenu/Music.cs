ea0db9da02fabbb6978fb8cd2aa36cd1fa576ee8
gustavogoul <gustavo_00goulart@hotmail.com>
2023-08-22 14:58:26 -0300

    MainMenu Changes, added options and quit (Options isn't implemented yet)

---
+++ Jogos digitais/Assets/Scripts/MainMenu/Music.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private float musicVolume = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.volume = musicVolume;
+    }
+
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
