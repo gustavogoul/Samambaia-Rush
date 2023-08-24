bfb1d4917d96262ccdbac10b577128d6b45b39e7
gustavogoul <gustavo_00goulart@hotmail.com>
2023-08-24 18:56:57 -0300

    MainMenu working

--- Jogos digitais/Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Jogos digitais/Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    public void BackToMenu()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -12,11 +21,14 @@ public class MainMenuScript : MonoBehaviour
 
     public void Options()
     {
-        
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
+
+    
 }
